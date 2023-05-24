3e9260fe60a92754d7ae99cfac36a03646385687
NafeesAhamed06 <75698313+NafeesAhamed06@users.noreply.github.com>
2023-05-24 18:29:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {})
 
-server.listen(3000, () => {
+server.listen(9001, () => {
     console.log("Listening on port 3000...")
 })
 
